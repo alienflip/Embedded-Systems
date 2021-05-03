a02776a7134ed4432a1b2a2997a8192092455df0
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-03 20:57:37 +0100

    Update FirstFrankenstein.ino

--- FirstFrankenstein.ino
+++ FirstFrankenstein.ino
@@ -1,17 +1,3 @@
-/*
-  SparkFun Inventor’s Kit
-  Circuit 4A-HelloWorld
-
-  The LCD will display the words "Hello World" and show how many seconds have passed since
-  the RedBoard was last reset.
-
-  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
-  This code is completely free for any use.
-
-  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
-  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
-*/
-
 #include <LiquidCrystal.h>          //the liquid crystal library contains commands for printing to the display
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);   // tell the RedBoard what pins are connected to the display
