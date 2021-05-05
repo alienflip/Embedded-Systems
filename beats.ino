ecd2e453228dfe1ee39a6667724820b34498fae6
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-05 09:40:08 +0100

    Update beats.ino

--- beats.ino
+++ beats.ino
@@ -1,32 +1,51 @@
+#include <Keypad.h>
+
+// speaker output pins
 const int sound_pin_0 = 12;
 const int sound_pin_1 = 13;
 
-int buttons[] = {2, 3, 4, 5, 6, 7, 8, 9};
+// sound variables
 int tones[] = {100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850};
 
-void setup() {
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+
+//define the cymbols on the buttons of the keypads: 0 -> S1, F -> S16
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','A','B'},
+  {'C','D','E','F'}
+};
+
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+  // declare sound pins as output
   pinMode(sound_pin_0, OUTPUT);
   pinMode(sound_pin_1, OUTPUT);
-
 }
+  
+void loop(){
+  // listen for keydown
+  char customKey = customKeypad.getKey();
 
-void loop() {
-  int pressed_btn = button_check();
-  if(pressed_btn != -1){
-    sound(sound_pin_0, tones[5]);
-    Serial.print(pressed_btn);
-  }
-}
-
-int button_check(){
-  for(int i = 2; i < 11; i++){
-    if(i == 11){
-      return -1;
-    }
-    if(digitalRead(i) == LOW){
-      return i;
-    }else{
-      continue;
+  // act on keydown
+  for(int i = 0; i < 4; i++){
+    for(int ii = 0; ii < 3; ii++){
+      if(customKey == hexaKeys[i][ii]){
+        Serial.println(customKey);
+        if(i < 4){
+          sound(sound_pin_0, tones[i * ii]);
+        }else{
+          sound(sound_pin_1, tones[i * ii]);
+        }
+      }
     }
   }
 }
