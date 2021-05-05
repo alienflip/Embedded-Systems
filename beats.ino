4ac8ac7bce6d180e1fd3ec644a3b5e1033260dd7
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-05 13:29:35 +0100

    Update beats.ino

--- beats.ino
+++ beats.ino
@@ -10,7 +10,7 @@ int tones[] = {100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 
-//define the cymbols on the buttons of the keypads: 0 -> S1, F -> S16
+//define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
   {'0','1','2','3'},
   {'4','5','6','7'},
@@ -24,13 +24,19 @@ byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
+// array records the current beat saved to the device: 16 bars
+char current_beat[16];
+
 void setup(){
   Serial.begin(9600);
   // declare sound pins as output
   pinMode(sound_pin_0, OUTPUT);
   pinMode(sound_pin_1, OUTPUT);
 }
-  
+
+// we assign key S16 -> '8' for record functionality, S15 -> 'C' for stop record, S14 -> for playback and S13 -> for delete recording
+bool record = false;
+
 void loop(){
   // listen for keydown
   char customKey = customKeypad.getKey();
@@ -40,10 +46,19 @@ void loop(){
     for(int ii = 0; ii < 4; ii++){
       if(customKey == hexaKeys[i][ii]){
         Serial.println(customKey);
-        if(i < 4){
-          sound(sound_pin_0, tones[i * ii]);
-        }else{
-          sound(sound_pin_1, tones[i * ii]);
+        switch(customKey){
+          case '8':
+            record = true;
+            break;
+          case 'C':
+            record = false;
+            break;
+          case '4':
+            playback();
+            break;
+          case '0':
+            delete_recording();
+            break;
         }
       }
     }
@@ -52,6 +67,14 @@ void loop(){
 
 void sound(int pin, int tones){
   tone(pin, tones);
-  delay(1000);
+  delay(32);
   noTone(pin);
+}
+
+void playback(){
+  
+}
+
+void delete_recording(){
+  
 }
